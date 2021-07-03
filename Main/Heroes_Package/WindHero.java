dcd053d505efbc33edc1034941183070d8b1af39
Erfan <erf80sami@gmail.com>
2021-07-03 10:44:02 +0430

    make WindHero Class

---
+++ Main/Heroes_Package/WindHero.java
@@ -0,0 +1,10 @@
+package Main.Heroes_Package;
+
+import Main.Team;
+
+public class WindHero extends Heroes{
+
+    public WindHero(Team team){
+        super(team , 800 , 2000 , Speed.FAST);
+    }
+}
