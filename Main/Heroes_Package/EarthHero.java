f607dbd1fae1aaa0e177ffba3ccc61161e5d9d13
Erfan <erf80sami@gmail.com>
2021-07-03 10:46:18 +0430

    make WindHero Class

---
+++ Main/Heroes_Package/EarthHero.java
@@ -0,0 +1,10 @@
+package Main.Heroes_Package;
+
+import Main.Team;
+
+public class EarthHero extends Heroes{
+
+    public EarthHero(Team team){
+        super(team , 1200 , 3000 , Speed.SLOW);
+    }
+}
