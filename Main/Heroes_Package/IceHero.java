94399e7de8d020c495aedb44f8d8f2ea5d1bc289
Erfan <erf80sami@gmail.com>
2021-07-03 10:39:41 +0430

    make IceHero Class

---
+++ Main/Heroes_Package/IceHero.java
@@ -0,0 +1,12 @@
+package Main.Heroes_Package;
+
+import Main.Team;
+
+public class IceHero extends Heroes{
+
+    public IceHero(Team team ){
+        super(team , 1000 , 2200 , Speed.SLOW );
+    }
+
+
+}
